54191dcba9d3494c78e7dad9e5324c3b0ae68e78
ycs1982 <yuri.engsoft2026@gmail.com>
2023-04-09 23:20:46 -0300

    alterei o enunciado

--- exercicio15.py
+++ exercicio15.py
@@ -1,1 +1,12 @@
+numero = int(input("Entre com um numero inteiro não negativo: "))
 
+if not (numero < 0):
+    if numero !=5:
+        resultado = numero ** (1/2)
+        print(resultado)
+    else:
+        resultado = numero ** (1 / 3)
+        print(resultado)
+
+if numero <=-1:
+    print("operação impossivel")
