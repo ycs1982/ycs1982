80795e1c3d88ec52e4763cfc4f2f25636b29207d
ycs1982 <yuri.engsoft2026@gmail.com>
2023-04-19 20:13:13 -0300

    adicionei o exercicio 19

--- exercicio19.py
+++ exercicio19.py
@@ -5,8 +5,13 @@ Ao final da apresentação do resultado, o programa deve perguntar ao usuário s
 novo cálculo. Se a resposta for sim, deve executar novamente as instruções subordinadas ao
 bloco adjacente. Se a resposta for não, o programa deve parar a execução
 """
-x = int(input("Entre com um valor acima de zero: "))
+
+resp = "sim"
 
 while True:
+    x = int(input("Entre com um valor acima de zero: "))
     resultado = x * 3
     print(resultado)
+    resposta = input("Deseja calcular novamente? Digite sim ou não: ")
+    if resposta != resp:
+        break
