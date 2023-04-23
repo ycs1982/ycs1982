184cc8a7439ffc5bbd2b16bf11d71e4e0cbe1797
ycs1982 <yuri.engsoft2026@gmail.com>
2023-04-22 23:42:15 -0300

    adicionei o exercicio 21

---
+++ exercicio21.py
@@ -0,0 +1,14 @@
+"""
+Elaborar um programa que efetue o cálculo da fatorial do valor inteiro 5
+e apresentar o resultado dessa operação
+"""
+
+
+fat = 1
+
+cont = 1
+
+while cont < 6:
+    fat = fat * cont
+    cont = cont + 1
+print(fat)
